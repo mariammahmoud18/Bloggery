b28c3d98b7ffec0c4bc2cb3befdce08b636d73e5
Mariam Swelam <62444533+mariammahmoud18@users.noreply.github.com>
2025-04-26 20:54:30 +0300

    Add files via upload

---
+++ bloggery/bloggery/src/main/java/com/bloggery/BloggeryApplication.java
@@ -0,0 +1,13 @@
+package com.bloggery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggeryApplication.class, args);
+	}
+
+}
