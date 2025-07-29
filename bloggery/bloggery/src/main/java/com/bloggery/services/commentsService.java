37d016bd8216c43e581b37ef64d28887a23e37bc
Mariam Swelam <mariammahmoud62001@gmail.com>
2025-07-29 21:44:36 +0300

    Update: Adding service layer

---
+++ bloggery/bloggery/src/main/java/com/bloggery/services/commentsService.java
@@ -0,0 +1,38 @@
+package com.bloggery.services;
+
+import com.bloggery.entities.Comments;
+import com.bloggery.repositories.commentsRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class commentsService {
+    commentsRepo repo;
+
+    public commentsService(commentsRepo repo) {
+        this.repo = repo;
+    }
+
+    public List<Comments> getAllComments(){
+        return repo.findAll();
+    }
+
+    public Comments getCommentById(int id){
+        return repo.findById(id).orElseThrow(()-> new RuntimeException("Comment Iid Not Found! - Please Enter a valid ID"));
+    }
+
+    public void updateComment(int id, String content){
+        Comments comment = getCommentById(id);
+        comment.setContent(content);
+        repo.save(comment);
+    }
+
+    public void saveComment(Comments comment){
+        repo.save(comment);
+    }
+
+    public void deleteCommentById(int id){
+        repo.deleteById(id);
+    }
+}
---
+++ bloggery/bloggery/src/main/java/com/bloggery/services/reactionsService.java
@@ -0,0 +1,4 @@
+package com.bloggery.services;
+
+public class reactionsService {
+}
