5dd1c984b743200a7f05df29527cdbfafc03b826
Mariam Swelam <mariammahmoud62001@gmail.com>
2025-11-15 23:58:03 +0200

    Update: Adding service layer to reactions & posts

--- bloggery/bloggery/src/main/java/com/bloggery/services/postsService.java
+++ bloggery/bloggery/src/main/java/com/bloggery/services/postsService.java
@@ -1,4 +1,54 @@
 package com.bloggery.services;
 
+import com.bloggery.entities.Posts;
+import com.bloggery.entities.Tags;
+import com.bloggery.repositories.postsRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class postsService {
+    postsRepo repo;
+
+    public postsService(postsRepo repo){
+        this.repo = repo;
+    }
+
+
+    public List<Posts> getAllPosts(){
+        return repo.findAll();
+    }
+
+    public Posts getPostById(int id){
+        return repo.findById(id).orElseThrow(()-> new RuntimeException("Post Id Not Found! - Please Enter a valid ID"));
+    }
+
+    public void updatePost(int id, String content, String title){
+        Posts post = getPostById(id);
+        post.setContent(content);
+        post.setTitle(title);
+        repo.save(post);
+    }
+
+    public void savePost(Posts post){
+        repo.save(post);
+    }
+
+    public void deletePostById(int id){
+        repo.deleteById(id);
+    }
+
+    public List<Posts> findPostsByUserId(int id){
+        return repo.findByUserId(id);
+    }
+
+    public List<Posts> findPostsByState(Posts.postState state){
+        return repo.findByState(state);
+    }
+
+    public List<Posts> findPostsByTageId(Tags tag){
+        return repo.findByTagIds(tag);
+    }
+
 }
--- bloggery/bloggery/src/main/java/com/bloggery/services/reactionsService.java
+++ bloggery/bloggery/src/main/java/com/bloggery/services/reactionsService.java
@@ -1,4 +1,38 @@
 package com.bloggery.services;
 
+import com.bloggery.entities.Reactions;
+import com.bloggery.entities.Users;
+import com.bloggery.repositories.reactionsRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class reactionsService {
+    reactionsRepo repo;
+
+    public reactionsService(reactionsRepo repo){
+        this.repo = repo;
+    }
+
+
+    public List<Reactions> getAllReactions(){
+        return repo.findAll();
+    }
+
+    public Reactions getReactionById(int id){
+        return repo.findById(id).orElseThrow(() -> new RuntimeException("User Id Not Found! - Enter a Valid ID"));
+    }
+
+    public void saveReaction(Reactions reaction){
+        repo.save(reaction);
+    }
+
+    public void deleteReactionById(int id){
+        repo.deleteById(id);
+    }
+
+    public List<Reactions> getReactionsByType(Reactions.reactionType type){
+        return repo.findByType(type);
+    }
 }
